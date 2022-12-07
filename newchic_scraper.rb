347871e59eb25feee0e22e11af119a745ac86cd9
Jonathan <jonitoadriano@gmail.com>
2022-12-07 15:35:32 -0600

    test byebug functionality and added it to method scraper

--- newchic_scraper.rb
+++ newchic_scraper.rb
@@ -1,10 +1,13 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+
 def scraper
   url = "https://www.newchic.com/hoodies-c-12200/1.html?newhead=0&mg_id=2&from=nav&country=223&NA=1"
   unparsed_html = HTTParty.get(url)
   page = Nokogiri::HTML(unparsed_html)
+  byebug 
 end
 
-p "test"
+scraper
+
