df8edfa64cdf7bde9d774da37319876139f7ab5f
Jonathan <jonitoadriano@gmail.com>
2022-12-07 15:25:07 -0600

    create new method scraper

---
+++ newchic_scraper.rb
@@ -0,0 +1,10 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+def scraper
+  url = "https://www.newchic.com/hoodies-c-12200/1.html?newhead=0&mg_id=2&from=nav&country=223&NA=1"
+  unparsed_html = HTTParty.get(url)
+  page = Nokogiri::HTML(unparsed_html)
+end
+
+p "test"
